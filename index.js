20eebf20f2d30d2196242f2690caad8913ed7cc3
Annie Zhou <annie_0812@live.ca>
2017-12-31 12:16:43 -0800

    input logic

--- index.js
+++ index.js
@@ -9,14 +9,44 @@ app.listen(3000, function(){
   });
   
   rl.on('line', (line) => {
-    console.log(line);
     processInput(line);
   });
-})
+});
+
+var specDir;
+var testDir;
+var lastNfiles;
 
 /* exists 3 types of usages for line:
-  
+  1. <int>
+  2. "specDir=" + <feature> (e.g. "specDir=EnrichedTimeOnCalculations)
+  3. "testDir=" + <test name> (e.g. "testDir=ET-out-of-context")
 */
 function processInput(line){
+  line = line.trim();
+  var input = parseInt(line);
   
+  if(isNaN(input)){
+    var idx = line.indexOf("=");
+    if(idx != 7){
+      console.log("please enter a valid input");
+      return;
+    }
+    var directory = line.substring(0, idx);
+    var name = line.substring(idx+1);
+    
+    if(directory === "specDir"){
+      specDir = name;
+      console.log(specDir);
+    }else if(directory === "testDir"){
+      testDir = name;
+      console.log(testDir);
+    }else{
+      console.log("please enter a valid input");
+      return;
+    }
+  }else{
+    lastNfiles = input;
+    console.log(lastNfiles);
+  }
 }
